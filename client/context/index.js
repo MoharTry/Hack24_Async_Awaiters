5494a4424d911b50044b331f56f58a1444d32cd8
MoharTry <mohartry@gmail.com>
2024-01-31 20:22:23 +0530

    Inital Commit

--- client/context/index.js
+++ client/context/index.js
@@ -1,51 +1,61 @@
-import React, {useEffect, useContext, createContext} from 'react'
+import React, { useEffect, useContext, createContext } from "react";
 
-import { useAddress, useContract, useMetamask, useContractWrite, useContractRead, useContractEvents } from "@thirdweb-dev/react";
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+  useContractRead,
+  useContractEvents,
+} from "@thirdweb-dev/react";
 
 import { ethers } from "ethers";
 
 const StateContext = createContext();
 
-
 export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0x1affbb90Ef26e3AB6117A411163A6fcEB57df90e"
+  );
 
-    const { contract } = useContract("0x1affbb90Ef26e3AB6117A411163A6fcEB57df90e");
-    
+  const address = useAddress();
+  const connect = useMetamask();
 
-    const address = useAddress();
-    const connect = useMetamask();
+  const realEstate = "Real Estate Dapp";
 
-    const realEstate = "Real Estate Dapp";
+  //FUNCTION-----
 
-    //FUNCTION-----
+  //1. listProperty()
 
-    //1. listProperty()
+  const {
+    mutateAsync: listProperty,
+    isLoading,
+    error,
+  } = useContractWrite(contract, "listProperty");
 
-    const { mutateAsync: listProperty, isLoading, error } = useContractWrite(contract, "listProperty");
-
-    const call = async () => {
-        try {
-            const data = await listProperty({
-                args: [
-                    owner,
-                    price,
-                    _propertyTitle,
-                    _category,
-                    _images,
-                    _pr
-                ]
-            })
-        }
+  const createPropertyFunction = async () => {
+    try {
+      const data = await listProperty({
+          [
+              address,
+              price,
+              _propertyTitle,
+              _category,
+              _images,
+              _propertyAddress,
+            _description,
+          ],
+      });
+      console.info("contract call successs", data);
+    } catch (err) {
+      console.error("contract call failure", err);
     }
-
+    };
     return (
-        <StateContext.Provider value={{ address, connect, contract, realEstate }}>
+        <StateContextProvider value={{ address, connect, contract, realEstate, createPropertyFunction }}>
             {children}
-        </StateContext.Provider>
+        </StateContextProvider>
     );
 };
 
-
 export const useStateContext = () => useContext(StateContext);
-
-
